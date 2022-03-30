9f87cca0b48416c3beddb36f3be5b93be9f4480b
SoaringClose <101990752+SoaringClose@users.noreply.github.com>
2022-03-30 21:41:01 +0300

    js

---
+++ js/script.js
@@ -0,0 +1,26 @@
+// // typeof
+
+// const type = false;
+// console.log(typeof (type));
+// // alert
+
+// console.log('До');
+// alert('hugkjh');
+// console.log('После');
+
+// confirm
+// const message = "Please confirm hotel reservation";
+// const isComing = confirm(message);
+
+// console.log(isComing);
+
+// PROMPT
+// let quantity = prompt('Введите количество товара');
+// quantity = Number(quantity);
+// console.log(quantity);
+// console.log(typeof (quantity));
+
+// NUMBER PARSEINT AND PARSEFLOAT
+let elementWidth = '50px';
+const result = Number.parseInt(elementWidth);
+console.log(typeof(result));
